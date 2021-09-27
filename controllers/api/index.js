1a63e8aaba5bc9955c771623457f16bde0c20431
Troy <troyat88@gmail.com>
2021-09-26 19:30:26 -0700

    more changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,9 @@ const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('/profile', userRoutes);
+router.use('/user', userRoutes);
 router.use('/post', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/comment', commentRoutes);
 
 
 module.exports = router;
