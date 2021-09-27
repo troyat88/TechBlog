020972aa9c3afc8afeb11bf30bf2843bef58c502
Troy <troyat88@gmail.com>
2021-09-26 18:10:32 -0700

    home, login, single post routes working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
+router.use('/profile', userRoutes);
+router.use('/post', postRoutes);
 router.use('/comments', commentRoutes);
 
 
